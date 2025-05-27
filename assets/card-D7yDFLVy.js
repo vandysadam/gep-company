d682831680633dbdb3857b0f2db213bf21f6817f
vandysdm <vandysdm@gmail.com>
2025-05-27 16:49:49 +0700

    Updates

--- assets/card-D7yDFLVy.js
+++ assets/card-D7yDFLVy.js
@@ -1,4 +1,4 @@
-import{c as o,r as d,j as s,e as t}from"./index-C0Fr0wOv.js";/**
+import{c as o,r as d,j as s,e as t}from"./index-BtDjf4BD.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
