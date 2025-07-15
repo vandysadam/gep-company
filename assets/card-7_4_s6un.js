5bc19327e2e9d0d826d0f0768f135a0d06164f7e
vandysdm <vandysdm@gmail.com>
2025-07-16 01:17:01 +0700

    Updates

--- assets/card-7_4_s6un.js
+++ assets/card-7_4_s6un.js
@@ -1,4 +1,4 @@
-import{c as o,r as d,j as s,k as t}from"./index-BcXMm6lw.js";/**
+import{c as o,r as d,j as s,k as t}from"./index-DJ76RukH.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
