e495f2574197e922d7bb560c02016ec4f747218b
vandysdm <vandysdm@gmail.com>
2025-07-17 01:18:04 +0700

    Updates

--- assets/about-screen-DqJa57si.js
+++ assets/about-screen-DqJa57si.js
@@ -1,4 +1,4 @@
-import{c as R,r as o,j as e,h as j,i as I,k as U,D as wa}from"./index-2dWF797Z.js";import{F as Ra,C as Ma,a as Sa}from"./card-Ca1Z2iKu.js";import{A as Ia}from"./aos-p284WFQn.js";/**
+import{c as R,r as o,j as e,h as j,i as I,k as U,D as wa}from"./index-Dndvl1Tf.js";import{F as Ra,C as Ma,a as Sa}from"./card-NLXPBwjt.js";import{A as Ia}from"./aos-B7Nl-jlo.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/card-NLXPBwjt.js
+++ assets/card-NLXPBwjt.js
@@ -1,4 +1,4 @@
-import{c as o,r as d,j as s,k as t}from"./index-2dWF797Z.js";/**
+import{c as o,r as d,j as s,k as t}from"./index-Dndvl1Tf.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
